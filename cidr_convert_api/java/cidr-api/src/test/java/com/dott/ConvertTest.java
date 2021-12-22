16384ef5a2515e00e4bb247ad93d2a616366fedb
caflorest <95430240+caflorest@users.noreply.github.com>
2021-12-21 20:48:06 -0600

    Update ConvertTest.java

--- cidr_convert_api/java/cidr-api/src/test/java/com/dott/ConvertTest.java
+++ cidr_convert_api/java/cidr-api/src/test/java/com/dott/ConvertTest.java
@@ -14,12 +14,12 @@ public class ConvertTest extends TestCase
         super( testName );
     }
 
-
+@test
     public static Test suite()
     {
         return new TestSuite( ConvertTest.class );
     }
-
+@test
     public void testValidCidrToMask()
     {
         assertEquals("128.0.0.0", Convert.cidrToMask("1"));
@@ -27,7 +27,7 @@ public class ConvertTest extends TestCase
         assertEquals("255.255.248.0", Convert.cidrToMask("21"));
         assertEquals("255.255.255.255", Convert.cidrToMask("32"));
     }
-
+@test
     public void testValidMaskToCidr()
     {
         assertEquals("1", Convert.maskToCidr("128.0.0.0"));
@@ -35,7 +35,7 @@ public class ConvertTest extends TestCase
         assertEquals("21", Convert.maskToCidr("255.255.248.0"));
         assertEquals("32", Convert.maskToCidr("255.255.255.255"));
     }
-
+@test
     public void testValidIpv4()
     {
         assertTrue(Convert.ipv4Validation("127.0.0.1"));
@@ -43,14 +43,14 @@ public class ConvertTest extends TestCase
         assertTrue(Convert.ipv4Validation("192.168.0.1"));
         assertTrue(Convert.ipv4Validation("255.255.255.255"));
     }
-
+@test
     public void testInvalidMaskToCidr()
     {
         assertEquals("Invalid", Convert.maskToCidr("0.0.0.0"));
         assertEquals("Invalid", Convert.maskToCidr("-1"));
         assertEquals("Invalid", Convert.maskToCidr("33"));
     }
-
+@test
     public void testInvalidCidrToMask()
     {
         assertEquals("Invalid", Convert.cidrToMask("0"));
@@ -58,7 +58,7 @@ public class ConvertTest extends TestCase
         assertEquals("Invalid", Convert.cidrToMask("255.255.255"));
         assertEquals("Invalid", Convert.cidrToMask("11.0.0.0"));
     }
-
+@test
     public void testInvalidIpv4()
     {
         assertFalse(Convert.ipv4Validation("192.168.1.2.3"));

0e78d6b17f1d0857f2ee00c90ee41358c3b09f14
caflorest <95430240+caflorest@users.noreply.github.com>
2021-12-21 21:41:20 -0600

    Update ConvertTest.java

--- cidr_convert_api/java/cidr-api/src/test/java/com/dott/ConvertTest.java
+++ cidr_convert_api/java/cidr-api/src/test/java/com/dott/ConvertTest.java
@@ -1,27 +1,20 @@
 package com.dott;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
+import static com.dott.Convert.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 public class ConvertTest extends TestCase
 {
-
     public ConvertTest( String testName )
     {
         super( testName );
     }
-
-@Test
     public static Test suite()
     {
         return new TestSuite( ConvertTest.class );
     }
-@Test
     public void testValidCidrToMask()
     {
         assertEquals("128.0.0.0", Convert.cidrToMask("1"));
@@ -29,7 +22,6 @@ public class ConvertTest extends TestCase
         assertEquals("255.255.248.0", Convert.cidrToMask("21"));
         assertEquals("255.255.255.255", Convert.cidrToMask("32"));
     }
-@Test
     public void testValidMaskToCidr()
     {
         assertEquals("1", Convert.maskToCidr("128.0.0.0"));
@@ -37,7 +29,6 @@ public class ConvertTest extends TestCase
         assertEquals("21", Convert.maskToCidr("255.255.248.0"));
         assertEquals("32", Convert.maskToCidr("255.255.255.255"));
     }
-@Test
     public void testValidIpv4()
     {
         assertTrue(Convert.ipv4Validation("127.0.0.1"));
@@ -45,14 +36,12 @@ public class ConvertTest extends TestCase
         assertTrue(Convert.ipv4Validation("192.168.0.1"));
         assertTrue(Convert.ipv4Validation("255.255.255.255"));
     }
-@Test
     public void testInvalidMaskToCidr()
     {
         assertEquals("Invalid", Convert.maskToCidr("0.0.0.0"));
         assertEquals("Invalid", Convert.maskToCidr("-1"));
         assertEquals("Invalid", Convert.maskToCidr("33"));
     }
-@Test
     public void testInvalidCidrToMask()
     {
         assertEquals("Invalid", Convert.cidrToMask("0"));
@@ -60,7 +49,6 @@ public class ConvertTest extends TestCase
         assertEquals("Invalid", Convert.cidrToMask("255.255.255"));
         assertEquals("Invalid", Convert.cidrToMask("11.0.0.0"));
     }
-@Test
     public void testInvalidIpv4()
     {
         assertFalse(Convert.ipv4Validation("192.168.1.2.3"));
@@ -69,3 +57,4 @@ public class ConvertTest extends TestCase
         assertFalse(Convert.ipv4Validation("...."));
     }
 }
+ 
